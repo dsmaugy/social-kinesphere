d1a101e8d727df300852fb4eec1b9483a1810212
Darwin Do <darwin78913@gmail.com>
2025-03-15 22:27:36 -0400

    dont start service right away

--- init/enable.sh
+++ init/enable.sh
@@ -7,4 +7,4 @@ mkdir -p ~/.config/systemd/user
 ln -s ~/social-kinesphere/init/$host.service ~/.config/systemd/user/void.service
 systemctl --user daemon-reload
 systemctl --user enable void.service
-systemctl --user start void.service
+# systemctl --user start void.service
